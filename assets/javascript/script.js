37ba09e38addf9d10441e64375fcc9bcb87e4ee5
geromuhaya <132812722+geromuhaya@users.noreply.github.com>
2023-10-24 07:47:08 +0100

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $('.menu-toggle').toggleClass('active');
+        $('.nav-bar').toggleClass('active');
+    })
+})
